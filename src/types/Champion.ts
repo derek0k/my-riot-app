5c3264f72e9dcf22c6289375789cf6c93bac78cb
derek0k <derek0kim@gmail.com>
2024-10-06 23:42:10 +0900

    types

---
+++ src/types/Champion.ts
@@ -0,0 +1,154 @@
+export type ChampionList = {
+  [key: string]: Champion;
+};
+
+export type Champion = {
+  version: string;
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  info: {
+    attack: number;
+    defense: number;
+    magic: number;
+    difficulty: number;
+  };
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  tags: string[];
+  partype: string;
+  stats: {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+  };
+};
+
+export type ChampionDetail = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  skins: {
+    id: string;
+    num: number;
+    name: string;
+    chromas: boolean;
+  }[];
+  lore: string;
+  blurb: string;
+  allytips: string[];
+  enemytips: string[];
+  tags: string[];
+  partype: string;
+  info: {
+    attack: number;
+    defense: number;
+    magic: number;
+    difficulty: number;
+  };
+  stats: {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+  };
+  spells: {
+    id: string;
+    name: string;
+    description: string;
+    tooltip: string;
+    leveltip: {
+      label: string[];
+      effect: string[];
+    };
+    maxrank: number;
+    cooldown: number[];
+    cooldownBurn: string;
+    cost: number[];
+    costBurn: string;
+    datavalues: {};
+    effect: (number[] | null)[];
+    effectBurn: string[];
+    vars: any[];
+    costType: string;
+    maxammo: string;
+    range: number[];
+    rangeBurn: string;
+    image: {
+      full: string;
+      sprite: string;
+      group: string;
+      x: number;
+      y: number;
+      w: number;
+      h: number;
+    };
+    resource: string;
+  }[];
+  passive: {
+    name: string;
+    description: string;
+    image: {
+      full: string;
+      sprite: string;
+      group: string;
+      x: number;
+      y: number;
+      w: number;
+      h: number;
+    };
+  };
+  recommended: any[];
+};
---
+++ src/types/Item.ts
@@ -0,0 +1,33 @@
+export type ItemList = {
+  [key: string]: Item;
+};
+
+export type Item = {
+  name: string;
+  description: string;
+  colloq: string;
+  plaintext: string;
+  into: string[];
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  gold: {
+    base: number;
+    purchasable: boolean;
+    total: number;
+    sell: number;
+  };
+  tags: string[];
+  maps: {
+    [key: string]: boolean;
+  };
+  stats: {
+    [key: string]: number;
+  };
+};
