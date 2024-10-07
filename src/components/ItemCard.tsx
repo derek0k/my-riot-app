8286384e1e72a86fcc73015c21bf0f5eddf63921
derek0k <derek0kim@gmail.com>
2024-10-07 13:59:33 +0900

    refac: ItemCard desc

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -9,12 +9,15 @@ type Props = {
 };
 
 export default function ItemCard({ href, imageSrc, title, desc }: Props) {
+  const displayDesc =
+    desc && desc.length > 24 ? `${desc.slice(0, 23)} ⋯` : desc;
+
   return (
-    <div className="border border-gray-500 rounded-sm">
-      <Link href={href} className="flex flex-col items-center p-2">
+    <div className="border border-gray-500 rounded-sm min-h-48 p-1">
+      <Link href={href} className="flex flex-col items-center gap-2 p-2">
         <Image priority src={imageSrc} alt={title} width={80} height={80} />
         <h2 className="text-lg font-bold text-red-400">{title}</h2>
-        <p className="text-gray-300">{desc}</p>
+        <p className="text-gray-300">{displayDesc}</p>
       </Link>
     </div>
   );
