a9343448f473c50f647e8eeb02e6fa5ed52cca45
derek0k <derek0kim@gmail.com>
2024-10-07 13:12:33 +0900

    Header z-index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 bg-section w-full py-6">
-      <nav className="container mx-auto flex justify-around">
+    <header className="fixed top-0 bg-section w-full py-6 z-50">
+      <nav className="container mx-auto flex justify-around font-semibold">
         {navLinks.map(({ href, label }) => (
           <Link href={href} key={label} className="hover:text-red-700">
             {label}
