03e7ca1dfe0f447e16e759007bbbd8fdfaa97b89
derek0k <derek0kim@gmail.com>
2024-10-07 12:36:36 +0900

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 bg-section w-full p-4">
+      <div className="mx-auto container text-foreground text-center text-sm">
+        © 2024 DĒRĒK - League of Legends
+      </div>
+    </footer>
+  );
+}
