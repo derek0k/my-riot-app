f6ffd0e26afc8efd237b48594fa2d3c05d7b5edb
derek0k <derek0kim@gmail.com>
2024-10-07 13:12:54 +0900

    style HomePage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,15 +4,26 @@ import MainCard from "@/components/MainCard";
 export default async function HomePage() {
   return (
     <>
-      <div className="text-center">
-        <h1>리그 오브 레전드 정보 앱</h1>
-        <p>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</p>
+      <div className="flex flex-col gap-4 text-center my-4">
+        <h1 className="text-4xl font-bold text-red-700">
+          리그 오브 레전드 정보 앱
+        </h1>
+        <p className="text-gray-500">
+          Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
+        </p>
       </div>
-      {navLinks
-        .filter((link) => link.label !== "홈")
-        .map(({ href, imageSrc, label }) => (
-          <MainCard key={label} label={label} href={href} imageSrc={imageSrc} />
-        ))}
+      <section className="flex flex-col gap-4 my-16">
+        {navLinks
+          .filter((link) => link.label !== "홈")
+          .map(({ href, imageSrc, label }) => (
+            <MainCard
+              key={label}
+              label={label}
+              href={href}
+              imageSrc={imageSrc}
+            />
+          ))}
+      </section>
     </>
   );
 }
--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 
 export default function MainCard({ label, href, imageSrc }) {
   return (
-    <Link href={href}>
-      <Image
-        src={imageSrc}
-        alt={label}
-        style={{ width: "100%", height: "auto" }}
-      />
+    <Link
+      href={href}
+      className="flex flex-col items-center gap-2 font-semibold text-xl text-amber-400 hover:text-amber-600"
+    >
+      <Image priority src={imageSrc} alt={label} width={450} height={450} />
       <div>{label}</div>
     </Link>
   );
